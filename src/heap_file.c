a0a5e7fc8c65c225f7a50b6903e485adb5274bd8
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-10-12 05:29:44 +0300

    private helper functions

--- src/heap_file.c
+++ src/heap_file.c
@@ -13,10 +13,69 @@
     return HP_ERROR;        \
   }                         \
 }
+#define MAX_BLOCK_RECORDS (BF_BLOCK_SIZE - 2 * sizeof(int)) / sizeof(Record)
+
+/***Private functions***/
 
 void _printRecord(Record record) {
     printf("Record: (%d %s %s %s)\n", record.id, record.name, record.surname, record.city);
 }
+
+void _getCharacteristic(char *block, int *characteristic) {
+    memcpy(characteristic, block, sizeof(int));
+}
+
+void _setCharacteristic(char *block, const int characteristic) {
+    memcpy(block, &characteristic, sizeof(int));
+}
+
+void _getUsedBlocks(char *block, int *usedBlocks) {
+    memcpy(usedBlocks, block + sizeof(int), sizeof(int));
+}
+
+void _setUsedBlocks(char *block, const int usedBlocks) {
+    memcpy(block + sizeof(int), &usedBlocks, sizeof(int));
+}
+
+void _getDataBlockNumber(char *block, int *dataBlockNumber) {
+    memcpy(dataBlockNumber, block + sizeof(int) * 2, sizeof(int));
+}
+
+void _setDataBlockNumber(char *block, const int dataBlockNumber) {
+    memcpy(block + sizeof(int) * 2, &dataBlockNumber, sizeof(int));
+}
+
+void _getCount(char *block, int *count) {
+    memcpy(count, block + BF_BLOCK_SIZE - 2 * sizeof(int), sizeof(int));
+}
+
+void _setCount(char *block, const int count) {
+    memcpy(block + BF_BLOCK_SIZE - 2 * sizeof(int), &count, sizeof(int));
+}
+
+void _getNext(char *block, int *next) {
+    memcpy(next, block + BF_BLOCK_SIZE - sizeof(int), sizeof(int));
+}
+
+void _setNext(char *block, const int next) {
+    memcpy(block + BF_BLOCK_SIZE - sizeof(int), &next, sizeof(int));
+}
+
+void _getRecord(char *block, const int offset, Record *record) {
+    memcpy(record, block + offset * sizeof(Record), sizeof(Record));
+}
+
+void _setRecord(char *block, const int offset, const Record *record) {
+    memcpy(block + offset * sizeof(Record), record, sizeof(Record));
+}
+
+int _isFull(char *block) {
+    int count = 0;
+    _getCount(block, &count);
+    return count == MAX_BLOCK_RECORDS;
+}
+
+/***Public functions***/
 
 HP_ErrorCode HP_Init() {
     return HP_OK;
